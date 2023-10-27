2991c866fd9176a5b2ab97fc0b594af43b111165
SuperCoder <propscode254@gmail.com>
2023-10-27 17:07:50 +0200

    5a (7) add context to App + Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeContext } from "./Context/Context";
+import { LoadingContext } from "./Context/Context";
+
+const App = () => {
+  const [theme, setTheme] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <section className={theme ? "dark" : null}>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+          <LoadingContext.Provider value={{ loading, setLoading }}>
+            {loading ? (
+              <BrowserRouter>
+                <Routes>
+                  {/* <Route path="/" element={}></Route>
+          <Route path="/gallery" element={}></Route> */}
+                </Routes>
+              </BrowserRouter>
+            ) : (
+              <LoadingPage />
+            )}
+          </LoadingContext.Provider>
+        </ThemeContext.Provider>
+      </section>
+    </>
+  );
+};
 
 export default App;

1abb78fdd10f639aa2d630d2ae0d9588be8f5cc0
SuperCoder <propscode254@gmail.com>
2023-10-28 15:57:57 +0200

    mission

--- src/components/Main/Mision.jsx
+++ src/components/Main/Mision.jsx
@@ -1,24 +1,25 @@
+import "./Mission.scss";
+
 const Mision = () => {
   return (
     <article className="mission-wrapper">
       <h2>Main Focus/Mission Statement</h2>
-      <div className="mission">
-        <span className="number">1</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
-          velit nulla architecto earum culpa enim distinctio error repellendus
-          reiciendis repellat, voluptates, ipsa incidunt doloribus cupiditate
-          hic. Magnam assumenda distinctio quo.
-        </p>
-      </div>
-      <div className="mission">
-        <span className="number">2</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
-          velit nulla architecto earum culpa enim distinctio error repellendus
-          reiciendis repellat, voluptates, ipsa incidunt doloribus cupiditate
-          hic. Magnam assumenda distinctio quo.
-        </p>
+      <div className="mi-wrapper">
+        <div className="mission">
+          <span className="number">1</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            efficitur, lectus et facilisis placerat.
+          </p>
+        </div>
+        <div className="mission">
+          <span className="number">2</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            efficitur, lectus et facilisis placerat, magna mauris porttitor
+            tortor, a auctor est felis ut nisl.
+          </p>
+        </div>
       </div>
     </article>
   );

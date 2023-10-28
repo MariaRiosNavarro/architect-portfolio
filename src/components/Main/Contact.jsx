f747fb4a1f426c07d00496566bd091fe42286942
SuperCoder <propscode254@gmail.com>
2023-10-28 17:04:29 +0200

    contact us

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,20 +1,64 @@
 import Button from "../Button/Button";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <article>
+    <article className="contact-wrapper">
       <h2>Contact Us</h2>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Phone Number" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Interessed in" />
-        <textarea type="text" placeholder="Message" />
-        <Button name="SEND EMAIL"></Button>
-      </form>
-      <div className="contact-img-wrapper">
-        <img src="/public/home/img10.jpeg" alt="someone at phone" />
+      <div className="contact-sm-wrapper">
+        <form>
+          <label htmlFor="name">
+            <h4>Name</h4>
+
+            <input type="text" name="name" id="name" />
+          </label>
+          <label htmlFor="phone">
+            <h4>
+              Phone Number<span>*</span>
+            </h4>
+
+            <input
+              type="text"
+              className="input-required"
+              name="phone"
+              id="phone"
+            />
+          </label>
+          <label htmlFor="email">
+            <h4>
+              Email<span>*</span>
+            </h4>
+
+            <input
+              type="text"
+              className="input-required"
+              name="email"
+              id="email"
+            />
+          </label>
+          <label htmlFor="interessed">
+            <h4>Interessed in</h4>
+            <input type="text" name="interessed" id="interessed" />
+          </label>
+          <label htmlFor="area">
+            <h4>
+              Message<span>*</span>
+            </h4>
+
+            <textarea
+              type="text"
+              name="area"
+              rows={12}
+              className="input-required"
+              id="area"
+            />
+          </label>
+        </form>
+        <div className="contact-img-wrapper">
+          <img src="/public/home/img10.jpeg" alt="someone at phone" />
+        </div>
       </div>
+      <Button name="SEND EMAIL"></Button>
     </article>
   );
 };

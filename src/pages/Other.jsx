5ddc8e1173ae2f111f7c68288cafe17f323e60a5
SuperCoder <propscode254@gmail.com>
2023-10-29 16:00:39 +0100

    fix

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -1,34 +1,17 @@
-// import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import "./Other.scss";
 
 const Other = () => {
-  //     const [count, setCount] = useState(0);
-  //     useEffect(() => {
-  //         console.log(count);
-  // }, [count]);
-
   return (
     <>
       <Header />
-      <section>
+      <section className="other-wrapper">
         <h2>In construction...</h2>
-        {/* <article>
-                        <h2>{props.property}</h2>
-                        <button onClick={() => {setCount(count + 1);}}>click +1</button>
-                        <p>{count}</p>
-                        <Link to='/'>See More</Link>
-                    </article> */}
       </section>
       <Footer />
     </>
   );
 };
-
-// Other.propTypes = {
-// property: PropTypes.string };
 
 export default Other;
